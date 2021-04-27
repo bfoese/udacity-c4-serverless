e7434ff9cf7e7bded95557f26b4fe11fa1db3e31
Britt Foese <britt.foese@gmail.com>
2021-04-27 14:45:09 +0200

    refactor(authorizer): fetch jwt certificate from public endpoint

--- backend/src/config/app.config.ts
+++ backend/src/config/app.config.ts
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 export const AppConfig = {
+  jsonWebKeySet: 'https://ataraxia.eu.auth0.com/.well-known/jwks.json',
   awsBucket: process.env.AWS_BUCKET,
   awsBucketSignedUrlExpiry: process.env.AWS_BUCKET_SIGNED_URL_EXPIRY,
   userIdIndex: process.env.User_ID_INDEX,
--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,7 +1,9 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda';
-import { verify } from 'jsonwebtoken';
+import { decode, verify } from 'jsonwebtoken';
+import fetch from 'node-fetch';
 import 'source-map-support/register';
 import { JwtPayload } from '../../auth/JwtPayload';
+import { AppConfig } from '../../config/app.config';
 import { createLogger } from '../../utils/logger';
 
 
@@ -10,27 +12,7 @@ const logger = createLogger('auth');
 // Done: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-// const jwksUrl = 'https://ataraxia.eu.auth0.com/.well-known/jwks.json';
-
-const cert = `-----BEGIN CERTIFICATE-----
-MIIDBTCCAe2gAwIBAgIJc/yZskRG1vjVMA0GCSqGSIb3DQEBCwUAMCAxHjAcBgNV
-BAMTFWF0YXJheGlhLmV1LmF1dGgwLmNvbTAeFw0yMTAxMDcxMjEwMDdaFw0zNDA5
-MTYxMjEwMDdaMCAxHjAcBgNVBAMTFWF0YXJheGlhLmV1LmF1dGgwLmNvbTCCASIw
-DQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMDB9Ewi12SfGE3UAticu9QEOsor
-w5BeaVLEVuwwt0ImiiisI7q7vsorhbApbkEJN91CtBbv4j/EQo4HSx8VGVPGvAU9
-iEEMKaVoc56gXaowemv6duzGcz3MOWJRRUX7DRBQwLMXgJ043LRnGTf2RZ92A4Id
-NoSKLNxf8n1rGpDpfCiw6de32aurvn/iGBv3Ue7WyRV5lo23GL0hjHZaUwJzyx77
-lSv3e6xPJWWiJ3ObFP/tSJG90KpHdYQtBx9wRjDmIjFACM+L94haqZ7AObCvPmU4
-BHYC+HfoWo1vo7AGI3PcDSDXL7Pyvj5kJ4LC9w2CyDv3bfBEkCcKQvL+3b8CAwEA
-AaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUgnLxpXjoko6KINWyuMn4
-ucV4mlkwDgYDVR0PAQH/BAQDAgKEMA0GCSqGSIb3DQEBCwUAA4IBAQCMTnWp7Nps
-/ud5Zg5CaJFmF2J1DlmoxTXN7YhOIsEF7moMeRVy4Ka+djbXmZwCaah0eTP3UnLH
-ef+TqJPmq3THyqV9ic8meyPQZVIyqzIu27TYj8YwBYhvOonwulQSOGYKTsbxDZEB
-MNAx2YpsgcVxgwXEajyrtQ9Jzw29lopX4GAspsCuvmLgwSr0R0T7GflXmclQxUOI
-wuTZ30DNkLUfQRAWgUyQP59LTu2Dn48dBl8zjFin4yPovqR/6NNOrxAJGiyoLB3j
-2V5w/VTTAzkPK7S/3/IcK9K6mhPS64xny8hgL5uSZVzgLRJQUgKViwu36rZiXOpK
-QczGuGEfJZbH
------END CERTIFICATE-----`;
+const jwksUrl = AppConfig.jsonWebKeySet;
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -72,9 +54,38 @@ export const handler = async (
   }
 }
 
+async function obtainCertificate(kid: string): Promise<{ alg: string, cert: string } | undefined> {
+  if (!kid) {
+    return undefined;
+  }
+  const result = await fetch(jwksUrl);
+
+  const json: Array<{}> = await result.json();
+  const certs = json ? ((json.keys as any) as []) : [];
+
+  const filtered = certs ? certs.filter(cert => (cert as any).kid === kid) : [];
+
+  if (filtered && filtered.length > 0) {
+    return { alg: filtered[0].alg, cert: `-----BEGIN CERTIFICATE-----\n${filtered[0].x5c}\n-----END CERTIFICATE-----` };
+  }
+  return undefined;
+}
+
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader);
-  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload;
+
+  let jwtKid;
+  try {
+    const jwt = token ? decode(token, { complete: true }) : undefined;
+    jwtKid = jwt ? (jwt as any).header.kid : undefined;
+  } catch {
+    throw new Error('Failed to parse jwt token');
+  }
+  const certData = await obtainCertificate(jwtKid);
+  if (!certData) {
+    throw new Error('No certificate to validate JWT available');
+  }
+  return verify(token, certData.cert, { algorithms: [certData.alg] }) as JwtPayload;
 }
 
 function getToken(authHeader: string): string {
